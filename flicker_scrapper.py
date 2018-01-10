f20fe06901517186b8e65be0999be8a2b035af86
dodler <tntlagf93@mail.ru>
2018-01-10 23:59:15 +0300

    some refactoring done

--- flicker_scrapper.py
+++ flicker_scrapper.py
@@ -3,40 +3,53 @@ import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-firefox_profile = webdriver.FirefoxProfile()
-firefox_profile.set_preference('permissions.default.image', 2)
-firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
 
-driver = webdriver.Firefox(firefox_profile=firefox_profile)
+def fetch_preview_urls(url, file_output, image_to_fetch):
+    """
+sample call
+fetch_preview_urls(
+    "https://www.flickr.com/search/?camera=motorola%2Fnexus_6&dimension_search_mode=min&height=3120&width=3120",
+    'output.txt', 500)
 
-driver.get("https://www.flickr.com/search/?camera=motorola%2Fnexus_6&dimension_search_mode=min&height=3120&width=3120")
+    :param url:
+    :param file_output: file with resulting urls
+    :param image_to_fetch:
+    :return:
+    """
+    firefox_profile = webdriver.FirefoxProfile()
+    firefox_profile.set_preference('permissions.default.image', 2)
+    firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
 
-images_to_collect = 500
+    driver = webdriver.Firefox(firefox_profile=firefox_profile)
 
-elm_len = 0
+    driver.get(url)
 
-while elm_len < images_to_collect:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    photos_class = "overlay"
+    images_to_collect = image_to_fetch
 
-    elms = driver.find_elements_by_class_name(photos_class)
-    print(len(elms))
-    elm_len = len(elms)
+    elm_len = 0
 
-    try:
-        text = 'Load more results'
-        btn = driver.find_element_by_class_name('infinite-scroll-load-more').\
-            find_element_by_tag_name('button')
-        btn.click()
-    except NoSuchElementException as e:
-        print(e)
+    while elm_len < images_to_collect:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        photos_class = "overlay"
 
-    time.sleep(1)
+        elms = driver.find_elements_by_class_name(photos_class)
+        print(len(elms))
+        elm_len = len(elms)
 
-hrefs = list(map(lambda e:e.get_attribute('href'), elms))
+        try:
+            text = 'Load more results'
+            btn = driver.find_element_by_class_name('infinite-scroll-load-more'). \
+                find_element_by_tag_name('button')
+            btn.click()
+        except NoSuchElementException as e:
+            print(e)
 
-with open('hrefs_output.txt','w') as f:
-    for h in hrefs:
-        f.write(h + '\n')
+        time.sleep(1)
 
-driver.quit()
+    hrefs = list(map(lambda e: e.get_attribute('href'), elms))
+
+    with open(file_output, 'w') as f:
+        for h in hrefs:
+            f.write(h + '\n')
+
+    driver.quit()
